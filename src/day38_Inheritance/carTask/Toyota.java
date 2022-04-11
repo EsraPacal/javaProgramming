b82aa33ab198fbbd80c35602f74451deddec1936
esra_pacal <esraksk.61@gmail.com>
2022-04-11 14:48:04 +0200

    Toyota class is completed

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,29 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car{
+
+    public Toyota(String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand+" "+model+" is reliable");
+    }
+
+
+}
+/*
+Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+
+			2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+
+			3. Tesla:
+					extra methods:
+						autoPilot();
+ */
