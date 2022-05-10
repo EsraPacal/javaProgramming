05f3bea8a35c995411f1f44ec5ad6c7bee511f24
esra_pacal <esraksk.61@gmail.com>
2022-05-10 15:38:04 +0200

    Eagle and Parrot classes with Flayable implement are completed

--- src/day44_Abstraction_Interface/animalTask/Parrot.java
+++ src/day44_Abstraction_Interface/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day44_Abstraction_Interface.animalTask;
 
-public final class Parrot extends Animal implements Playable{
+public final class Parrot extends Animal implements Playable, Flyable{
 
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -14,6 +14,11 @@ public final class Parrot extends Animal implements Playable{
     @Override
     public void play() {
         System.out.println(getName()+" is playing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName()+" can fly20km/h ");
     }
 }
 
